1d85a601fba3c34e82d498e07b369154f1d0ae73
creasedigital <ojorechris@gmail.com>
2022-05-02 10:40:37 +0100

    react-redux setup complete

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,19 @@
+const CakeContainer = () => {
+	return (
+		<div style={{ width: "100vw", margin: "0px auto" }}>
+			<h2 style={{ fontSize: "2rem", color: "sienna" }}>Number of cakes</h2>
+			<button
+				style={{
+					fontSize: "2rem",
+					backgroundColor: "transparent",
+					cursor: "pointer",
+					color: "sienna",
+					border: "2px solid sienna",
+				}}
+			>
+				Buy Cake
+			</button>
+		</div>
+	);
+};
+export default CakeContainer;
